e2244ea68ddb6ad821e673ba3cad907d1e8940c4
RasmusAC <rasmus@supera.dk>
2021-04-15 12:12:52 +0200

    Tilføjet kommentarer

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,9 +6,9 @@ $dBUserName = "root";
 $dBPassword = "";
 $dBName = "beskedsystem";
 
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // viser SQL fejl istedet for gennerel PHP fejl
-$conn = mysqli_connect($serverName,$dBUserName,$dBPassword,$dBName);
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Viser SQL fejl istedet for generel PHP fejl.
+$conn = mysqli_connect($serverName,$dBUserName,$dBPassword,$dBName); // Tilslutter til databasen med de givne variabler.
 
 if (!$conn) {
-    die("Connection Failed: " . mysqli_connect_error());
+    die("Connection Failed: " . mysqli_connect_error()); // Hvis det ikke opstår en forbindelse, så afslut forbindelsen og viser fejlen.
 }
