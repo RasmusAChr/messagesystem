9c1f09162b641ec6476c9c6f87cdd0148ce862fe
Zibrandtsen <3449943@rts-365.dk>
2021-04-18 20:30:22 +0200

    MAN KAN ENDELIG SENDE BESKEDER & TIL FLERE BRUGERE

--- includes/sendbeskeder.inc.php
+++ includes/sendbeskeder.inc.php
@@ -2,20 +2,22 @@
 
 if (isset($_POST["submit"])) {
 
-    $modtager = $_POST["modtager"];
+    $modtagere = $_POST["modtager"];
+    $modtagere_array = array_map('trim', preg_split("/,/", $modtagere));
+
     $besked = $_POST["besked"];
     
     require_once 'dbh.inc.php';
     require_once 'functions.inc.php';
 
     // Tjekker for fejl (error handelers)
-    if (emptyInputLogin($modtager, $besked) != false) {
+    if (emptyInputLogin($modtagere, $besked) != false) {
         header("location: ../sendbeskeder.php?error=emptyinput");
         exit();
     }
 
     // Send besked
-    sendMessage($conn, $modtager, $besked);
+    sendMessage($conn, $modtagere_array, $besked);
 }
 else {
     header("location: ../sendbeskeder.php");
